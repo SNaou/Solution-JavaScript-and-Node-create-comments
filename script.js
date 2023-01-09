bf0d65c42de245b7a304dea2108829b0d8687920
SNaou <S.Naou@icloud.com>
2023-01-09 16:04:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log("Hello Winc Academy students")
+
+//this is a large sum
+/*
+console.log(1230941 * 1823794);
+
+console.log(637263 / 54);
+
+*/
+
+//you wil not see it in node because it is in a comment block so it is disabled.
+
